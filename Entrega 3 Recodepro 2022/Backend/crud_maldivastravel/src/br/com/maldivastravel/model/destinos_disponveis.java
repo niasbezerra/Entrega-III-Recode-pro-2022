6a2470ae9a0e92cbbdc36fde16e8d3e7246baad4
niasbezerra <bezerranias@gmail.com>
2022-10-13 17:10:56 -0300

    Atualizacoes
    criação das class restantes

---
+++ Entrega 3 Recodepro 2022/Backend/crud_maldivastravel/src/br/com/maldivastravel/model/destinos_disponveis.java
@@ -0,0 +1,20 @@
+package br.com.maldivastravel.model;
+
+public class destinos_disponveis {
+	private int id_destino_disponivel;
+	private String destino;
+	
+	public int getId_destino_disponivel() {
+		return id_destino_disponivel;
+	}
+	public void setId_destino_disponivel(int id_destino_disponivel) {
+		this.id_destino_disponivel = id_destino_disponivel;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+}
