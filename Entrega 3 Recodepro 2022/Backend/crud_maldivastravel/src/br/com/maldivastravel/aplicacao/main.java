0f58ee166cbcbe9148b4ffb45059188431d36893
niasbezerra <bezerranias@gmail.com>
2022-10-13 15:33:06 -0300

    correçoes

--- Entrega 3 Recodepro 2022/Backend/crud_maldivastravel/src/br/com/maldivastravel/aplicacao/main.java
+++ Entrega 3 Recodepro 2022/Backend/crud_maldivastravel/src/br/com/maldivastravel/aplicacao/main.java
@@ -11,10 +11,10 @@ public class main {
 		cliente cliente = new cliente();
 		
 
-		cliente.setNome("Beyonce Knowless");
-		cliente.setEmail("beyonce@gmail.com");
-		cliente.setSenha("32569874"); 
-		cliente.setDestino_interesse("recife");
+		cliente.setNome("Madonna Delafuente");
+		cliente.setEmail("bDelafuentee@gmail.com");
+		cliente.setSenha("9632145"); 
+		cliente.setDestino_interesse("sao paulo");
 		
 		clienteDAO.save(cliente);
 
